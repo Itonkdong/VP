0a4cf13867832b0e76a041e8ac9052e9be92bc74
Itonkdong <kostadinoskiviktor@yahoo.com>
2024-12-10 21:21:35 +0100

    lab 3

--- lab1-B/lab/src/main/java/mk/ukim/finki/mk/lab/repository/jpa/AlbumRepository.java
+++ lab1-B/lab/src/main/java/mk/ukim/finki/mk/lab/repository/jpa/AlbumRepository.java
@@ -4,8 +4,10 @@ import mk.ukim.finki.mk.lab.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long>
 {
-
+//    Optional<Album> findById(Long id);
 }
